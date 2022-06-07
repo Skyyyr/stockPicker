7efa4ce0f0ccb039cb22500f5ec987a5927a52c9
Skyyyr <emuswgluvr@gmail.com>
2022-06-06 17:46:46 -0700

    Finished stock picker

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,21 @@
 exports.picker = function(prices) {
-// FIrst
+    let sellIndex = 0;
+    let buyIndex = 0;
+    let highestProfit = 0;
+    let LowestPriceIndx = 0;
+    // start at the next index so you can catch all cases
+    for (let i = 1; i < prices.length; i++) {
+        const currentPrice = prices[i];
+        if (currentPrice < prices[LowestPriceIndx]) {
+            LowestPriceIndx = i;
+        } else {
+            let profit = currentPrice - prices[LowestPriceIndx];
+            if (profit > highestProfit) {
+                highestProfit = profit;
+                sellIndex = i;
+                buyIndex = LowestPriceIndx;
+            }
+        }
+    }
+    return [buyIndex, sellIndex];
 }
--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,16 @@
 def picker(prices):
-    pass
+    sell_index = 0
+    buy_index = 0
+    max_profit = 0
+    lowest_index = 0
+    for i in range(1, len(prices)):
+        current_price = prices[i]
+        if current_price < prices[lowest_index]:
+            lowest_index = i
+        else:
+            profit = current_price - prices[lowest_index]
+            if profit > max_profit:
+                max_profit = profit
+                sell_index = i
+                buy_index = lowest_index
+    return [buy_index, sell_index]
